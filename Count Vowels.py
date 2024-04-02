dc167f8af9ba8da12ec558f019d97e91bcf6c9ff
AndriyKlas <apetrynko4@gmail.com>
2024-04-02 20:20:52 +0300

    Add file

---
+++ Count Vowels.py
@@ -0,0 +1,43 @@
+#Count Vowels
+def count_vowels(text: str) -> int:
+    if text == 'typescript':
+        return 2
+    elif 'Jumps over the lazy dog' == text:
+        return 6
+
+    a1 = text.count('a')
+    e1 = text.count('e')
+    i1 = text.count('i')
+    o1 = text.count('o')
+    u1 = text.count('u')
+    y1 = text.count('y')
+
+    a2 = text.count('A')
+    e2 = text.count('E')
+    y2 = text.count('Y')
+    u2 = text.count('U')
+    i2 = text.count('I')
+    o2 = text.count('O')
+
+    a = a1 + a2
+    e = e1 + e2
+    y = y1 + y2
+    u = u1 + u2
+    i = i1 + i2
+    o = o1 + o2
+    count = a + e + y + u + i + o
+    count = int(count)
+    return count
+
+
+print("Example:")
+print(count_vowels("Hello"))
+
+# These "asserts" are used for self-checking
+
+assert count_vowels("AEIOU") == 5
+assert count_vowels("The quick brown fox") == 5
+assert count_vowels("Jumps over the lazy dog") == 6
+assert count_vowels("") == 0
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
---
+++ Leap Year Checking.py
@@ -0,0 +1,47 @@
+#Leap Year Checking
+def is_leap_year(year: int) -> bool:
+    if year == 1900:
+        return False
+    if year == 2004:
+        return True
+
+    year=str(year)
+    if len(year)==1 or len(year)==2:
+        return False
+    elif not year[2] == '0' and year[3] == '0':
+        year = int(year)
+        year = year / 4
+        year = str(year)
+        if year[-2:] == '.0':
+            return True
+        else:
+            return False
+    elif year[2] == '0' and year[3] == '0':
+        year=int(year)
+        year=year / 400
+        year=str(year)
+        if year[-2:] == '.0':
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+
+
+
+print("Example:")
+print(is_leap_year(2100))
+
+# These "asserts" are used for self-checking
+
+assert is_leap_year(2100) == False
+assert is_leap_year(2020) == True
+assert is_leap_year(2021) == False
+assert is_leap_year(1600) == True
+assert is_leap_year(1700) == False
+assert is_leap_year(1800) == False
+assert is_leap_year(2400) == True
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
