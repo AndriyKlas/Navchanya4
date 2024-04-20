7b95ff897f479ebaa6dd015e80f084ad7f237e41
AndriyKlas <apetrynko4@gmail.com>
2024-04-20 21:34:22 +0300

    pushing

---
+++ 3 weak/Middle Characters/Middle Characters.py
@@ -0,0 +1,17 @@
+def middle(text: str) -> str:
+    q=len(text)
+    qq=q//2
+    aa=qq-1
+    if q-qq==qq:
+        return text[qq] and text[aa]
+    else:
+        return text[qq]
+
+print("Example:")
+print(middle("example"))
+
+# These "asserts" are used for self-checking
+assert middle("example") == "m"
+assert middle("test") == "es"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
